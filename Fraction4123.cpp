60942ef71cae97f955d4261c75c9d519f0651e14
Samuel Mullins <samuel.s.mullins@gmail.com>
2018-01-29 14:36:25 -0600

    Removed int x from parameterized constructor because i wasnt using it

--- Fraction4123.cpp
+++ Fraction4123.cpp
@@ -10,7 +10,6 @@ Fraction4123::Fraction4123()
 //2.  Imprement parameterized constructor
 //    Set num and den to values passed through parameters - Samuel Mullins
 Fraction4123::Fraction4123(int n, int d){
-	int x;
 	num = n;
 	if (d != 0)
 		den = d;
